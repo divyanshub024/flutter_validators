e9921edbda36095e6b6c131acc265d73a98218ac
Divyanshu Bhargava <divyanshub024@gmail.com>
2021-09-07 21:45:42 +0530

    Added email validator

---
+++ lib/src/validators/email.dart
@@ -0,0 +1,14 @@
+/// Check if string is a valid email.
+bool isEmail(String str) => _isEmail(str);
+
+extension EmailX on String {
+  bool get isEmail {
+    return _isEmail(this);
+  }
+}
+
+bool _isEmail(String str) {
+  return RegExp(
+          r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$')
+      .hasMatch(str);
+}
